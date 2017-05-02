026e243c92053ac46a1d83c8a801bd5cb76de6d1
钟晶 <zhongjing@zillionfortune.com>
2017-05-02 15:40:34 +0800

    添加RocketMQ

---
+++ rocketmq/rocketmq-consumer/src/main/java/com/example/middleware/rocketmq/RocketmqConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.middleware.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqConsumerApplication.class, args);
+	}
+}
