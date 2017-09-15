b48178b412e71d0e7ccba2955428ee1caa102478
钟晶 <zhongjing@zillionfortune.com>
2017-09-15 16:27:01 +0800

    添加dubbo example

--- dubbo/dubbo-provider/src/main/java/com/example/dubbo/DubboProviderApplication.java
+++ dubbo/dubbo-provider/src/main/java/com/example/dubbo/DubboProviderApplication.java
@@ -2,9 +2,11 @@ package com.example.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.dubbo")
 @ImportResource("classpath:dubbo-provider.xml")
 public class DubboProviderApplication {
 
