dfc5ae6bb704abfa78b714490b8e88f2c6216a54
zhongjing <zhongjing@zillionfortune.com>
2017-12-26 10:52:40 +0800

    整理项目结构

---
+++ rocketmq/rocketmq-provider/src/main/java/com/example/middleware/rocketmq/RocketmqProviderApplication.java
@@ -0,0 +1,12 @@
+package com.example.middleware.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqProviderApplication.class, args);
+	}
+}
